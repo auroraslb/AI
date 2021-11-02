aa99b18643531169ffe4bc01be41ceac97b653a0
Karoline Herleiksplass <karolshe@stud.ntnu.no>
2021-11-02 15:49:16 +0000

    first

--- assignment2/search_solution.py
+++ assignment2/search_solution.py
@@ -1,10 +1,13 @@
 from typing import Tuple
 from numpy import array
 import search
+import numpy as np
+from math import dist, pi, sin, cos
 
 # you can want to use the class registration_iasd
 # from your solution.py (from previous assignment)
 from solution import registration_iasd
+
 
 # Choose what you think it is the best data structure
 # for representing actions.
@@ -12,7 +15,20 @@ Action = None
 
 # Choose what you think it is the best data structure
 # for representing states.
-State = None
+
+
+class State():
+
+    def __init__(self, x_min = -180, x_max = 180, y_min = -180, y_max = 180, z_min = -180, z_max = 180):
+        self.x_min = x_min
+        self.x_max = x_max
+        self.y_min = y_min
+        self.y_max = y_max
+        self.z_min = z_min
+        self.z_max = z_max
+
+        self.R = np.eye(3)
+        self.t = [0,0,0]
 
 
 class align_3d_search_problem(search.Problem):
@@ -34,7 +50,12 @@ class align_3d_search_problem(search.Problem):
         # Creates an initial state.
         # You may want to change this to something representing
         # your initial state.
-        self.initial = None 
+
+        self.scan1 = scan1
+        self.scan2 = scan2
+
+        self.initial = State()
+        
 
         return
 
@@ -52,8 +73,19 @@ class align_3d_search_problem(search.Problem):
         :return: Tuple with all possible actions
         :rtype: Tuple
         """
+        available_actions = ()
+        
+        if state.x_min != state.x_max:
+            available_actions = available_actions + ('x+',) +('x-',)
+        
+        if state.y_min != state.y_max:
+            available_actions = available_actions + ('y+',) +('y-',)
 
-        pass
+        if state.z_min != state.z_max:
+            available_actions = available_actions + ('z+',) +('z-',)
+
+        return available_actions
+
 
 
     def result(
@@ -72,8 +104,54 @@ class align_3d_search_problem(search.Problem):
         :return: A new state
         :rtype: State
         """
-        
-        pass
+
+        print(action, type(action))
+
+        x_min = state.x_min
+        x_max = state.x_max
+        y_min = state.y_min
+        y_max = state.y_max
+        z_min = state.z_min
+        z_max = state.z_max
+
+        if action[0] == 'x':
+            if action[1] == '-':
+                x_min = x_min
+                x_max = x_min + (x_max - x_min)//2
+            else:
+                x_max = x_max
+                x_min = x_min + (x_max - x_min)//2
+
+        if action[0] == 'y':
+            if action[1] == '-':
+                y_min = y_min
+                y_max = y_min + (y_max - y_min)//2
+            else:
+                y_max = y_max
+                y_min = y_min + (y_max - y_min)//2
+
+        if action[0] == 'z':
+            if action[1] == '-':
+                z_min = z_min
+                z_max = z_min + (z_max - z_min)//2
+            else:
+                z_max = z_max
+                z_min = z_min + (z_max - z_min)//2
+
+        return State(x_min, x_max, y_min, y_max, z_min, z_max)
+
+    def _distance(self, scan1, scan2):
+        distances = 0
+
+        for point in scan1:
+            distance_vec = np.linalg.norm(scan2-point, axis = 1)
+            min = np.amin(distance_vec)
+            distances += min**2
+
+        print(np.sqrt(distances))
+
+        return np.sqrt(distances)
+
 
 
     def goal_test(
@@ -90,8 +168,45 @@ class align_3d_search_problem(search.Problem):
         :return: returns true or false, whether it represents a node state or not
         :rtype: bool
         """
-        
-        pass
+        print('x_min:', state.x_min, 'x_max:', state.x_max)
+        print('y_min:', state.y_min, 'y_max:', state.y_max)
+        print('z_min:', state.z_min, 'z_max:', state.z_max)
+
+        x_avg = (state.x_min + (state.x_max - state.x_min)/2) * pi/180
+        y_avg = (state.y_min + (state.y_max - state.y_min)/2) * pi/180
+        z_avg = (state.z_min + (state.z_max - state.z_min)/2) * pi/180
+
+        rot_x = [[1, 0, 0, 0, cos(x_avg), -sin(x_avg), 0, sin(x_avg), cos(x_avg)]]
+        rot_y = [[cos(y_avg), 0, sin(y_avg), 0, 1, 0, -sin(y_avg), 0, cos(y_avg)]]
+        rot_z = [[cos(z_avg), -sin(z_avg), 0, sin(z_avg), cos(z_avg), 0, 0, 0, 1]]
+
+        rot_x = np.array(rot_x).reshape((3,3))
+        rot_y = np.array(rot_y).reshape((3,3))
+        rot_z = np.array(rot_z).reshape((3,3))
+
+        rot_avg = np.dot(rot_x, np.dot(rot_y, rot_z))
+
+        scan1_updated = np.dot(self.scan1,rot_avg)
+
+        reg = registration_iasd(scan1_updated, self.scan2)
+
+        R, t = reg.get_compute()
+
+        new_r = np.dot(R, rot_avg)
+
+        rotated_scan1 = np.dot(self.scan1, new_r)
+
+        scan1_sorted = np.sort(rotated_scan1)
+        scan2_sorted = np.sort(self.scan2)
+
+        if self._distance(rotated_scan1, self.scan2) < 0.5:
+        #np.allclose(scan1_sorted, scan2_sorted, rtol=0.5, atol=0.05):
+        #self._distance(rotated_scan1, self.scan2) < 1:
+            state.R = new_r
+            state.t = t
+            return True
+
+        return False
 
 
     def path_cost(
@@ -141,5 +256,9 @@ def compute_alignment(
         solution in the proposes search tree.
     :rtype: Tuple[bool, array, array, int]
     """
+    output = search.breadth_first_graph_search(align_3d_search_problem(scan1, scan2))
+
+    print(output!=None)
+
+    return output!=None, output.state.R, output.state.t, output.depth
     
-    pass
--- assignment2/solution.py
+++ assignment2/solution.py
@@ -1,28 +1,21 @@
 from registration import registration
 from get_pointcloud import point_cloud_data
 
+import numpy as np
 from typing import Tuple
-
-from copy import deepcopy
+from math import sqrt
 
 
 class registration_iasd(registration):
 
-    def __init__(
-            self,
-            scan_1: array((..., 3)),
-            scan_2: array((..., 3)),
-            ) -> None:
+    def __init__(self, scan_1: np.array((..., 3)), scan_2: np.array((..., 3))) -> None:
 
         # inherit all the methods and properties from registration
         super().__init__(scan_1, scan_2)
 
         return
 
-    def compute_pose(
-            self,
-            correspondences: dict
-            ) -> Tuple[array, array]:
+    def compute_pose(self,correspondences: dict) -> Tuple[np.array, np.array]:
         """compute the transformation that aligns two
         scans given a set of correspondences
 
@@ -31,7 +24,43 @@ class registration_iasd(registration):
         :return: rotation and translation that align the correspondences
         :rtype: Tuple[np.array, np.array]
         """
-        pass
+
+        pi = []
+        qi = []
+
+        for value in correspondences.keys():
+            pi.append(correspondences[value]['point_in_pc_1'])
+            qi.append(correspondences[value]['point_in_pc_2'])
+
+        #Get the center point of S1 
+        p_average = np.mean(pi, axis=0)
+        #Get the center point of S2
+        q_average = np.mean(qi, axis=0)
+
+        #Frame aligned with the center of S1
+        p_aligned = pi - p_average
+        #Frame aligned with the center of S2
+        q_aligned = qi - q_average
+
+        #Stack all ep i in a matrix of dimension N x 3
+        P = np.stack( p_aligned, axis=0 )
+        #Stack all eq i in a matrix of dimension N x 3
+        Q = np.stack( q_aligned, axis=0 )
+
+        #Get matrix A of dimension 3 x 3
+        A = (Q.transpose()).dot(P)
+
+        #U, Sum, and Vt using SVD, all of dimension 3 x 3
+        U, Sum, V_t = np.linalg.svd(A, full_matrices=True)
+        
+        #Get the output rotation
+        R_out = (U.dot(np.diag([1,1,np.linalg.det(U.dot(V_t))]))).dot(V_t)
+        
+        #Get the output translation
+        t_out = q_average - R_out.dot(p_average)
+        
+        #Return tuple of R_out and t_out
+        return R_out, t_out
 
 
     def find_closest_points(self) -> dict:
@@ -42,10 +71,44 @@ class registration_iasd(registration):
         Points in scan 2 do not have necessarily a correspondence.
 
         :return: a dictionary with the correspondences. Keys are numbers identifying the id of the correspondence.
-                Values are a dictionaries with 'point_in_pc_1', 'point_in_pc_2' identifying the pair of points in the correspondence.
+                    Values are a dictionaries with 'point_in_pc_1', 'point_in_pc_2' identifying the pair of points in the correspondence.
         :rtype: dict
         """
-        pass
+        
+        # Dictionary of correspondance
+        correspondance = {}
+
+        # Compute sum of squares for both point clouds (needed for finding closest point later)
+        # ie. get x**2 + y**2 + z**2
+        square_sum_scan_1 = np.sum(np.square(self.scan_1),axis=1)
+        square_sum_scan_2 = np.sum(np.square(self.scan_2),axis=1)
+        
+        point_nb_scan_1 = 0
+
+        # for every point in cloud 1
+        for point_1 in self.scan_1: #np.array, dim (N,3)
+
+            # Distance is root of 
+            # (x_1-x_2)**2 + (y_1-y_2)**2 + (z_1-z_2)**2
+            # = x_1**2 + x_2**2 - 2*x_1*x_2 + ...
+            # For current point in scan 1, calculate distances to points in scan 2
+            dot_product = 2*np.dot(self.scan_1[point_nb_scan_1], np.transpose(self.scan_2))
+            distance = np.sqrt((square_sum_scan_1[point_nb_scan_1] + square_sum_scan_2 - dot_product))
+            
+            # Find index of smallest distance, ie. closest point
+            index_closest_point = np.argmin(distance, axis=0)
+            closest_point = self.scan_2[index_closest_point]
+            closest_dist = distance[index_closest_point]
+
+            # Add to correspondance, using the number of the point in scan 1 as key
+            correspondance[point_nb_scan_1] = { 'point_in_pc_1': point_1, 
+                                                'point_in_pc_2': closest_point,
+                                                'dist2': sqrt(closest_dist)
+                                              }
+            
+            point_nb_scan_1 += 1
+    
+        return correspondance
 
 
 class point_cloud_data_iasd(point_cloud_data):
@@ -53,7 +116,6 @@ class point_cloud_data_iasd(point_cloud_data):
     def __init__(
             self,
             fileName: str,
-            apply_rand_transform: bool=False
             ) -> None:
             
         super().__init__(fileName)
@@ -65,12 +127,48 @@ class point_cloud_data_iasd(point_cloud_data):
             self,
             file: str
             ) -> bool:
+
         """Loads a point cloud from a ply file
 
         :param file: source file
         :type file: str
-        :return: returns true or false, depending on wether the method
+        :return: returns true or false, depending on whether the method
         the ply was OK or not
         :rtype: bool
         """
-        pass
+
+        lines = []
+        with open(file) as f:
+            lines = f.readlines()
+
+        reading_vertices = False
+
+        for line_number, line in enumerate(lines, 0):
+            if 'element vertex' in line:
+                nb_vertices = int(line.split()[2])
+                first_property_line = line_number+1
+
+            if 'property float x' in line:
+                x = line_number - first_property_line
+            elif 'property float y' in line:
+                y = line_number - first_property_line
+            elif 'property float z' in line:
+                z = line_number - first_property_line
+
+            if reading_vertices:
+                nb_vertices = nb_vertices-1
+                if nb_vertices == 0:
+                    reading_vertices = False
+                properties = line.split()
+
+                try:
+                    coordinates = [float(properties[int(x)]), float(properties[int(y)]), float(properties[int(z)])]
+                    self.data[line_number-first_vertice_line] = coordinates
+                except:
+                    return False
+            
+            if 'end_header' in line:
+                reading_vertices = True
+                first_vertice_line = line_number+1
+
+        return True
