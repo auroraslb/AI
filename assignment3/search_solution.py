cd6e88a89e295cdaf7a3b49e2cc65074cc3cdec0
Aurora Bjørlo <aurorasb@stud.ntnu.no>
2021-11-10 18:33:58 +0000

    Clean up in code, current solution gets 28/64

--- assignment3/search_solution.py
+++ assignment3/search_solution.py
@@ -3,6 +3,7 @@ from numpy import array
 import search
 import numpy as np
 from math import pi, sin, cos
+import random
 
 # you can want to use the class registration_iasd
 # from your solution.py (from previous assignment)
@@ -29,9 +30,6 @@ class State():
 
         self.R = np.eye(3)
         self.t = [0,0,0]
-    
-    def node_print(self):
-        print("State: ", self.x_min, " ", self.x_max, " ", self.y_min, " ", self.y_max, " ", self.z_min, " ", self.z_max)
 
 
 class align_3d_search_problem(search.Problem):
@@ -43,7 +41,6 @@ class align_3d_search_problem(search.Problem):
             ) -> None:
         """Module that instantiate your class.
         You CAN change the content of this __init__ if you want.
-
         :param scan1: input point cloud from scan 1
         :type scan1: np.array
         :param scan2: input point cloud from scan 2
@@ -70,13 +67,12 @@ class align_3d_search_problem(search.Problem):
         """Return the actions that can be executed in the given state.
         The result would be a list, since there are only four possible actions
         in any given state of the environment
-
         :param state: Abstract representation of your state
         :type state: State
         :return: Tuple with all possible actions
         :rtype: Tuple
         """
-        #"""
+
         available_actions = ()
         
         if state.x_min != state.x_max:
@@ -87,10 +83,6 @@ class align_3d_search_problem(search.Problem):
 
         if state.z_min != state.z_max:
             available_actions = available_actions + ('z+',) +('z-',)
-        #"""
-
-        #available_actions = ['x+','x-','y+','y-','z+','z-']
-
 
         return available_actions
 
@@ -104,7 +96,6 @@ class align_3d_search_problem(search.Problem):
         """Return the state that results from executing the given
         action in the given state. The action must be one of
         self.actions(state).
-
         :param state: Abstract representation of your state
         :type state: [type]
         :param action: An action
@@ -155,13 +146,6 @@ class align_3d_search_problem(search.Problem):
             distances += min**2
 
         return np.sqrt(distances)
-    
-    def _distance_point(self, point1, scan2):
-
-        distance_vec = np.linalg.norm(scan2-point1, axis = 1)
-        #min = np.amin(distance_vec)
-
-        return np.amin(distance_vec) #np.sqrt(distances)
 
     def goal_test(
             self,
@@ -171,16 +155,11 @@ class align_3d_search_problem(search.Problem):
         state to self.goal or checks for state in self.goal if it is a
         list, as specified in the constructor. Override this method if
         checking against a single self.goal is not enough.
-
         :param state: gets as input the state
         :type state: State
         :return: returns true or false, whether it represents a node state or not
         :rtype: bool
         """
-
-        #gamma = (state.x_min + (state.x_max - state.x_min)/2) * pi/180
-        #beta = (state.y_min + (state.y_max - state.y_min)/2) * pi/180
-        #alpha = (state.z_min + (state.z_max - state.z_min)/2) * pi/180
 
         x_mid = (state.x_min + (state.x_max - state.x_min)/2) * pi/180
         y_mid = (state.y_min + (state.y_max - state.y_min)/2) * pi/180
@@ -192,32 +171,15 @@ class align_3d_search_problem(search.Problem):
 
         rotation_matrix = np.matmul(rot_z, np.matmul(rot_y, rot_x))
 
-        #rotation_matrix = np.array([[cos(alpha)*cos(beta), cos(alpha)*sin(beta)*sin(gamma)-sin(alpha)*cos(gamma), cos(alpha)*sin(beta)*cos(gamma)+sin(alpha)*sin(gamma)], 
-        #                    [sin(alpha)*cos(beta), sin(alpha)*sin(beta)*sin(gamma)+cos(alpha)*cos(gamma), sin(alpha)*sin(beta)*cos(gamma)-cos(alpha)*sin(gamma)], 
-        #                    [-sin(beta), cos(beta)*sin(gamma), cos(beta)*cos(gamma)]])
-        #Much slower
-
         scan1_updated = np.matmul(rotation_matrix, self.scan1.T).T
+        
+        scan1_sample = random.choices(scan1_updated, k = int(len(scan1_updated)*0.1))
 
-        rng = np.random.default_rng()
-        scan1_sample = rng.choice(scan1_updated, int(len(scan1_updated)*0.1))
-        print(scan1_sample)
-        #scan2_sample = rng.choice(self.scan2, len(self.scan2)//10)
-
-        #if self._distance_point(scan1_updated, self.scan2) < 0.01:
-        #if self._distance_point(scan1_updated[np.random.randint(0,len(scan1_updated))], self.scan2) < 0.01:
-        #scan1_average = np.mean(scan1_updated, axis=0)
-        #scan2_average = np.mean(self.scan2, axis=0)
-        #print(np.linalg.norm(scan1_average-scan2_average))
-        #if np.linalg.norm(scan1_average-scan2_average) < 1e-17:
         if self._distance(scan1_sample, self.scan2) < 0.5:
-            #print(scan1_average)
-            #print(scan2_average)
 
             reg = registration_iasd(scan1_updated, self.scan2)
 
             R, t = reg.get_compute()
-            print("x_mid: ", x_mid, ", y_mid: ", y_mid, " , z_mid: ", z_mid)
 
             num_points, _ = scan1_updated.shape
             scan1_updated = (
@@ -225,8 +187,7 @@ class align_3d_search_problem(search.Problem):
                             np.matmul(t.reshape((3,1)),np.ones((1,num_points)))
                             ).T
 
-            rng2 = np.random.default_rng()
-            scan1_updated = rng2.choice(scan1_updated, int(len(scan1_updated)*0.75))
+            scan1_updated = random.choices(scan1_updated, k = int(len(scan1_updated)*0.75))
             
             if self._distance(scan1_updated, self.scan2) < 0.1:
                 state.R = np.matmul(R, rotation_matrix)
@@ -248,7 +209,6 @@ class align_3d_search_problem(search.Problem):
         is such that the path doesn't matter, this function will only look at
         state2. If the path does matter, it will consider c and maybe state1
         and action. The default method costs 1 for every step in the path.
-
         :param c: cost to get to the state1
         :type c: [type]
         :param state1: parent node
@@ -283,7 +243,6 @@ def compute_alignment(
     """Function that will return the solution.
     You can use any UN-INFORMED SEARCH strategy we study in the
     theoretical classes.
-
     :param scan1: first scan of size (..., 3)
     :type scan1: array
     :param scan2: second scan of size (..., 3)
@@ -302,14 +261,8 @@ def compute_alignment(
     scan1_aligned = scan1 - scan1_average
     scan2_aligned = scan2 - scan2_average
 
-    output = search.breadth_first_graph_search(align_3d_search_problem(scan1_aligned, scan2_aligned)) #scan2_aligned
+    output = search.breadth_first_graph_search(align_3d_search_problem(scan1_aligned, scan2_aligned))
 
-    print('R:', output.state.R.shape) # (3,3)
-    print('t', output.state.t.shape) # (3,)
-    print('aligned', scan1_aligned.shape) #(756,3)
-    print('average', scan1_average.shape)
-
-    #translation = output.state.t + np.dot(output.state.R, scan1_average.T) - scan1_average - scan1_average + scan2_average  # - np.dot(np.linalg.inv(output.state.R), scan1_average.T).T + scan2_average
     translation = - np.dot(output.state.R, scan1_average.T) + output.state.t + scan2_average
 
     return output!=None, output.state.R, translation, output.depth
